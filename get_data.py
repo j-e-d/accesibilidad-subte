5ea039882e5cd8dbdc1fb38767141646831e17df
j-e-d <j-e-d@users.noreply.github.com>
2022-07-20 01:01:25 -0300

    Remove date column

--- get_data.py
+++ get_data.py
@@ -4,42 +4,45 @@ import operator
 import requests
 
 
-result = requests.get("https://aplicacioneswp.metrovias.com.ar/APIAccesibilidad/Accesibilidad.svc/GetLineas")
+result = requests.get(
+    "https://aplicacioneswp.metrovias.com.ar/APIAccesibilidad/Accesibilidad.svc/GetLineas"
+)
 json_data = result.json()
 
 results = list()
 for line in json_data:
     data_dict = {
-        'idLinea': line['idLinea'],
-        'nombreLinea': line['nombre'].strip(),
-        'fueraDeHorario': line['fueraDeHorario']
+        "idLinea": line["idLinea"],
+        "nombreLinea": line["nombre"].strip(),
+        "fueraDeHorario": line["fueraDeHorario"],
     }
-    for station in line['estaciones']:
+    for station in line["estaciones"]:
         station_dict = data_dict | {
-            'idEstacion': station['idEstacion'],
-            'nombreEstacion': station['nombre'].strip()
+            "idEstacion": station["idEstacion"],
+            "nombreEstacion": station["nombre"].strip(),
         }
-        for access in station['accesos']:
+        for access in station["accesos"]:
             access_dict = station_dict | {
-                'cabecera': access['cabecera'],
-                'descripcion': access['descripcion'].strip(),
-                'fechaActualizacion': access['fechaActualizacion'],
-                'fechaNormalizacion': access['fechaNormalizacion'],
-                'funcionando': access['funcionando'],
-                'nombre': access['nombre'].strip(),
-                'sentido': access['sentido'],
-                'tipo': access['tipo']
+                "cabecera": access["cabecera"],
+                "descripcion": access["descripcion"].strip(),
+                # 'fechaActualizacion': access['fechaActualizacion'],
+                "fechaNormalizacion": access["fechaNormalizacion"],
+                "funcionando": access["funcionando"],
+                "nombre": access["nombre"].strip(),
+                "sentido": access["sentido"],
+                "tipo": access["tipo"],
             }
             results.append(access_dict)
 
 
-results.sort(key=operator.itemgetter('nombre'))
-results.sort(key=operator.itemgetter('idEstacion'))
-results.sort(key=operator.itemgetter('idLinea'))
+results.sort(key=operator.itemgetter("nombre"))
+results.sort(key=operator.itemgetter("idEstacion"))
+results.sort(key=operator.itemgetter("idLinea"))
 
-with open('data.csv', 'w', encoding='utf8', newline='') as output_file:
-    fc = csv.DictWriter(output_file,
-                        fieldnames=results[0].keys(),
-                       )
+with open("data.csv", "w", encoding="utf8", newline="") as output_file:
+    fc = csv.DictWriter(
+        output_file,
+        fieldnames=results[0].keys(),
+    )
     fc.writeheader()
     fc.writerows(results)
